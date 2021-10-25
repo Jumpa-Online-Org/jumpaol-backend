67734cfbc34036c3f8db08f6a6007624e1816ae6
falihnaufal17 <falihnaufal1700@gmail.com>
2021-10-25 09:25:09 +0700

    fix: count total page

--- helper.js
+++ helper.js
@@ -2,16 +2,16 @@ const db = require('./src/models')
 const User = db.users
 
 module.exports = {
-    getPagination: (page, size) => {
-        const limit = size ? +size : 10
-        const offset = page ? page * limit : 0
+    getPagination: (page, size = 10) => {
+        const limit = +size
+        const offset = page ? (page * limit) - +size : 0
 
         return { limit, offset }
     },
     getPagingData: (datas, page, limit) => {
         const { count: total, rows: data } = datas
         const current_page = page ? +page : 1
-        const total_pages = Math.ceil(total / limit) - 1
+        const total_pages = Math.ceil(total / limit)
         
         return { total, data, total_pages, current_page, per_page: limit }
     },
